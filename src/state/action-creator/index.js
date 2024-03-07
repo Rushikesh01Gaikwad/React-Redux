a8d9a435e0400c029c31032b3d271ebc655b9cac
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2024-03-07 16:27:40 +0530

    first change in redux

---
+++ src/state/action-creator/index.js
@@ -0,0 +1,27 @@
+export const depositMoney=(amount)=>
+{
+    return(dispatch)=>
+    {
+        dispatch
+        (
+            {
+                type:'deposit',
+                payload: amount
+            }
+        )
+    }
+}
+
+export const withdrowMoney=(amount)=>
+{
+    return(dispatch)=>
+    {
+        dispatch
+        (
+            {
+                type:'withdrow',
+                payload: amount
+            }
+        )
+    }
+}
